d926e39150d24c0ab519e3c4fb8255becfbdb606
张兴柱 <mr.francisco@foxmail.com>
2018-01-21 18:03:35 +0800

    wireMock服务伪造，便于提前交付前端

---
+++ src/main/java/com/lesson/spring/web/support/MockServer.java
@@ -0,0 +1,19 @@
+package com.lesson.spring.web.support;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class MockServer {
+
+
+
+    public static void main(String[] args){
+        configureFor("127.0.0.1",8080);
+        stubFor(get(urlEqualTo("/book"))
+                .willReturn(okJson("{'name':'tom'}"))
+
+        );
+
+    }
+
+
+}
